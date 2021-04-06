f825d59c6006f136d580a2e8a91ccc8a1f420e63
wilsonmike <michaelwilsonsj@hotmail.com>
2021-04-06 16:03:24 -0400

    heroku endpoints

--- backend/yfw.api.js
+++ backend/yfw.api.js
@@ -4,6 +4,8 @@ const pool = require("./connection");
 
 //api routes including pg data
 
+//api routes including pg data
+
 //TVSO Data
 yfw.get('/amazon', function(req,res) {
     let query = `select * from amazon`;
@@ -12,5 +14,39 @@ yfw.get('/amazon', function(req,res) {
     });
 })
 
+yfw.get('/etsy', function(req,res) {
+    let query = `select * from etsy`;
+    pool.query(query).then((response) => {
+        res.json(response.rows);
+    });
+})
+
+yfw.get('/tvso', function(req,res) {
+    let query = `select * from tvso`;
+    pool.query(query).then((response) => {
+        res.json(response.rows);
+    });
+})
+yfw.get('/manualorders', function(req,res) {
+    let query = `select * from manualorders`;
+    pool.query(query).then((response) => {
+        res.json(response.rows);
+    });
+})
+
+//costume data
+yfw.get('/maskmarket', function(req,res) {
+    let query = `select * from maskmarket`;
+    pool.query(query).then((response) => {
+        res.json(response.rows);
+    });
+})
+
+yfw.get('/costumeagent', function(req,res) {
+    let query = `select * from costumeagent`;
+    pool.query(query).then((response) => {
+        res.json(response.rows);
+    });
+})
 
 module.exports = yfw;
